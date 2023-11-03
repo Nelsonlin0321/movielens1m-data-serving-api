99440d98608ad2422603ac1ebf7e0dc53dad132f
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-11-04 01:28:22 +0800

    update the test

--- tests/test_cloud_api.py
+++ tests/test_cloud_api.py
@@ -6,7 +6,7 @@ URL = "https://jh9d4gng3e.execute-api.ap-southeast-1.amazonaws.com/prod"
 def test_api():
     page_size = 12
     response = requests.get(
-        f"{URL}/api/v1/movies/?page_size={page_size}").json()
+        f"{URL}/api/v1/movies/?page_size={page_size}", timeout=10).json()
     assert len(response['results']) == page_size
 
 
@@ -14,7 +14,7 @@ def test_search():
     limit = 30
     q = "black"
     response = requests.get(
-        f"{URL}/api/v1/movies/search?q={q}&limit={limit}").json()
+        f"{URL}/api/v1/movies/search?q={q}&limit={limit}", timeout=10).json()
 
     assert response.status_code == 200
 
